95d0e893da9843a7525ab3f0f11ecb2ecb684767
Mariia Koval <m.kovalmaria@gmail.com>
2025-03-18 20:06:30 +0200

    change lint

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import usersFromServer from './api/users';
 
 const initialTodo: Todo[] = todosFromServer.map(todo => ({
   ...todo,
-  user: usersFromServer.find(user => user.id === todo.userId),
+  user: usersFromServer.find(user => user.id === todo.userId) || null,
 }));
 
 function getNewTodoId(todos: Todo[]) {
